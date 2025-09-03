4dbb19c703848b6d38de6da79f8ffe93920e7d36
Magda <magdaverocas@gmail.com>
2025-09-03 14:00:21 -0600

    Creación de login

---
+++ Nica-Turismo/JavaScript/Code.js
@@ -0,0 +1,30 @@
+//página de inicio
+function acceder() {
+    let login = document.getElementById("login");
+    //let signup = document.getElementById("signup");
+
+}
+
+//Login
+function Loguear() {
+    let usuario = document.getElementById("usuario").value;
+    let contraseña = document.getElementById("clave").value;
+
+    if (usuario == "Vero" && contraseña == "contraseña123") {
+        window.location = "paciente.html";
+    }
+    else {
+        alert("Datos incorrectos");
+    }
+}
+
+//sign up
+//function registrar() {
+  //  const nuevoUsuario = document.getElementById('nuevo-usuario').value;
+  //  const nuevaClave = document.getElementById('nueva-clave').value;
+
+  //  const registroMessage = document.getElementById('registro-message');
+  //  registroMessage.textContent = 'Registro exitoso. Ahora puedes iniciar sesión.';
+  //  registroMessage.style.color = 'green';
+
+//}
